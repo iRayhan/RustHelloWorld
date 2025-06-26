1faa68914af81e1e30f14dd87809638b65fce9e5
irayhan <jbc.rayhan@gmail.com>
2025-06-26 14:48:52 +0600

    struct, enums and pattern matching

---
+++ src/main.rs
@@ -0,0 +1,200 @@
+use std::sync::Arc;
+
+fn send() {
+    let s = String::from("hello");
+
+    // send the ownership of s to different thread
+    std::thread::spawn(move || {
+        println!("send {}", s);
+    });
+}
+
+fn sync() {
+    let s = Arc::new(String::from("hello"));
+    let s1 = Arc::clone(&s);
+
+    // share the ownership of s with s1 and send to different thread
+    std::thread::spawn(move || {
+        println!("sync {}", s1);
+    });
+
+    // s can still be used in the main thread
+    println!("{}", s);
+}
+
+fn transfer_ownership(s: String) {
+    println!("transfer_ownership {}", s);
+}
+
+fn borrow_ownership(s: &String) {
+    println!("borrow_ownership {}", s);
+}
+
+#[derive(Default, Debug)]
+struct Father {
+    debug: bool,
+    retries: u32,
+    timeout: f64,
+    child: Child,
+}
+
+#[derive(Debug)]
+struct Child {
+    debug: bool,
+    retries: u32,
+    timeout: f64,
+}
+
+impl Default for Child {
+    fn default() -> Self {
+        Self {
+            debug: false,
+            retries: 8,
+            timeout: 3.0,
+        }
+    }
+}
+
+impl Child {
+    fn new(self: Self) -> Self {
+        Self { ..self }
+    }
+    fn get_debug(&self) -> bool {
+        self.debug
+    }
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String, i32),
+    ChangeColor(u8, u8, u8),
+    Other(Child),
+    // Text("jbc"),
+    JBC,
+}
+
+fn process_message(msg: Message) {
+    use Message::*;
+
+    match &msg {
+        Quit => println!("Quit"),
+        Move { x, y } => println!("Move to x: {}, y: {}", x, y),
+        Write(text, number) => println!("Text: {}", text),
+        ChangeColor(r, g, b) => println!("Change color to RGB({}, {}, {})", r, g, b),
+        _ => println!("other"),
+    }
+
+    if let Write(t, _) = msg {
+        println!("Write to x: {}", t);
+    }
+}
+
+fn main() {
+    // transfer ownership
+    let a = String::from("2");
+    transfer_ownership(a);
+
+    // borrow ownership with read permission
+    let a = String::from("2");
+    let b = &a;
+    let c = &a;
+    borrow_ownership(b);
+    borrow_ownership(c);
+
+    // borrow ownership with read and write permission
+    let mut a = String::from("2");
+    let b = &mut a;
+    b.push_str("3");
+    borrow_ownership(b);
+
+    // send the ownership to different thread
+    send();
+
+    // share the ownership with different thread
+    sync();
+
+    // struct
+    let father = Father::default();
+    println!("{:?}", father);
+
+    let child = Child {
+        retries: 2,
+        debug: false,
+        timeout: 3.0,
+    };
+    child.debug;
+    
+    // enums
+    let message = Message::Write(String::from("hello"), 5);
+
+    // pattern
+    let (_, x, ..) = (1, "sdfdsaf", 10, 23, 22);
+
+    // *a = String::from("3");
+
+    // println!("{}{}", a, b);
+    /*
+    // filter string list
+    let mut name_list = Vec::new();
+    name_list.push("a");
+    name_list.push("b");
+    let filtered_name: Vec<&&str> = name_list.iter().filter(|&name| !name.eq(&"a")).collect();
+    // println!("{:?}", filtered_name);
+
+    // filter number list
+    let mut number_list = Vec::new();
+    number_list.push(1);
+    number_list.push(2);
+    number_list.push(3);
+    let filtered_number: Vec<&i32> = number_list.iter().filter(|&number| number == &3).collect();
+    // println!("{:?}", filtered_number);
+
+    // create object
+    let person = Person {
+        name: "Jon".to_string(),
+        age: 25,
+        profile: Profile {
+            name: "Engineer".to_string(),
+            experience: 5
+        }
+    };
+    // println!("{:?}", person);
+    // println!("{:#?}", person);
+
+    // Integer addition
+    // println!("1 + 2 = {}", 1u32 + 2);
+
+    // return function
+    // println!("1 + 2 = {}", add(1, 2));
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+    println!("names: {:?}", names);*/
+}
+
+fn some_number() -> Option<u32> {
+    Some(42)
+}
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: i8,
+    profile: Profile,
+}
+
+#[derive(Debug)]
+struct Profile {
+    name: String,
+    experience: i8,
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
