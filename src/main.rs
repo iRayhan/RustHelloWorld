20bc7900ec12fdf956396cdb908bfe60ee167d95
irayhan <jbc.rayhan@gmail.com>
2025-07-02 12:57:47 +0600

    enhance generics: implement traits for grades and error handling, refactor `get_grade` with conditional logic

--- src/main.rs
+++ src/main.rs
@@ -112,28 +112,80 @@ fn return_custom_result(s: String) -> Result<i32, CustomError> {
     }
 }
 
-struct GenericStruct<T> {
-    grade: T,
+trait AllowedGrade {
+    fn to_int(&self) -> i32;
+}
+impl AllowedGrade for String {
+    fn to_int(&self) -> i32 {
+        -1
+    }
+}
+impl AllowedGrade for &str {
+    fn to_int(&self) -> i32 {
+        -1
+    }
+}
+impl AllowedGrade for i32 {
+    fn to_int(&self) -> i32 {
+        *self
+    } 
+}
+impl AllowedGrade for f32 {
+    fn to_int(&self) -> i32 {
+        *self as i32
+    }
 }
 
-fn get_grade<T>(grade: GenericStruct<T>) -> String
+trait AllowedError {
+    fn from_bool(value: bool) -> Self;
+}
+impl AllowedError for bool {
+    fn from_bool(value: bool) -> Self {
+        value
+    }
+}
+
+struct GenericStruct<T, E>
 where
-    T: std::fmt::Display,
+    T: AllowedGrade,
+    E: AllowedError,
+{
+    grade: T,
+    error: E,
+}
+
+fn get_grade<T, E>(mut grade: GenericStruct<T, E>) -> Option<String>
+where
+    T: std::fmt::Display + AllowedGrade + std::cmp::PartialOrd,
+    E: std::fmt::Display + AllowedError,
 {
     match std::any::type_name::<T>() {
-        "alloc::string::String" | "&str" => format!("String grade: {}", grade.grade),
-        "f64" => format!("Float grade: {}", grade.grade),
-        "i32" => format!("Integer grade: {}", grade.grade),
-        _ => format!("Unknown type grade: {}", grade.grade)
+        "alloc::string::String" | "&str" => {
+            grade.error = E::from_bool(false);
+            Some(format!("String grade, error: {} {}", grade.grade, grade.error))
+        },
+        "i32" => {
+            if grade.grade.to_int() > 100 {
+                grade.error = E::from_bool(true);
+                return Some(format!("Integer grade, error: {} {}", grade.grade, grade.error))
+            }
+            Some(format!("Integer grade, error: {} {}", grade.grade, grade.error))
+        }
+        "f32" => {
+            if grade.grade.to_int() > 100 {
+                grade.error = E::from_bool(true);
+                return Some(format!("Float grade, error: {} {}", grade.grade, grade.error))
+            }
+            Some(format!("Float grade, error: {} {}", grade.grade, grade.error))
+        },
+        _ => None,
     }
 }
 
+
+
 fn get_larger<T: PartialOrd>(a: T, b: T) -> T {
-    if a > b {
-        a
-    } else {
-        b
-    }
+    if a > b { a } else { b }
 }
 
 fn main() {
@@ -173,7 +225,7 @@ fn main() {
 
     // enums
     let message = Message::Write(String::from("hello"), 5);
-    return_result(5);
+    println!("{:?}", return_result(5));
 
     // pattern
     let (_, x, ..) = (1, "sdfdsaf", 10, 23, 22);
@@ -212,10 +264,13 @@ fn main() {
         println!("{:?}", y.error_code);
         println!("{:?}", y.error_msg);
     }
-    
+
     // generics
-    let a = GenericStruct { grade: true };
-    println!("{}", get_grade(a));
+    let a = GenericStruct { 
+        grade: 101.5,
+        error: false,
+    };
+    println!("{}", get_grade(a).unwrap_or(String::from("unknown")));
 
     // *a = String::from("3");
 
