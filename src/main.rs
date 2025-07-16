cfc9e722d051240fbac4ebe8634c61c2d227acd8
irayhan <jbc.rayhan@gmail.com>
2025-07-16 11:52:44 +0600

    sample test

--- src/main.rs
+++ src/main.rs
@@ -282,9 +282,72 @@ struct TestLinkedList {
     pointer: Box<RefCell<Option<TestLinkedList>>>
 }
 
+fn addition(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_addition_positive_numbers() {
+        assert_eq!(addition(2, 3), 5);
+        assert_eq!(addition(10, 15), 25);
+    }
+
+    #[test]
+    fn test_addition_negative_numbers() {
+        assert_eq!(addition(-2, -3), -5);
+        assert_eq!(addition(-10, 5), -5);
+    }
+
+    #[test]
+    fn test_addition_zero() {
+        assert_eq!(addition(0, 5), 5);
+        assert_eq!(addition(5, 0), 5);
+        assert_eq!(addition(0, 0), 0);
+    }
+
+    #[test]
+    fn test_addition_large_numbers() {
+        assert_eq!(addition(1000000, 2000000), 3000000);
+    }
+}
+
+#[cfg(test)]
+mod tests1 {
+    use super::*;
+
+    #[test]
+    fn test_addition_positive_numbers() {
+        assert_eq!(addition(2, 3), 5);
+        assert_eq!(addition(10, 15), 25);
+    }
+
+    #[test]
+    fn test_addition_negative_numbers() {
+        assert_eq!(addition(-2, -3), -5);
+        assert_eq!(addition(-10, 5), -5);
+    }
+
+    #[test]
+    fn test_addition_zero() {
+        assert_eq!(addition(0, 5), 5);
+        assert_eq!(addition(5, 0), 5);
+        assert_eq!(addition(0, 0), 0);
+    }
+
+    #[test]
+    fn test_addition_large_numbers() {
+        assert_eq!(addition(1000000, 2000000), 3000000);
+    }
+}
+
+
 fn main() {
 
-    let a = TestLinkedList {
+/*    let a = TestLinkedList {
         data: String::from("a"),
         pointer: Box::new(RefCell::new(None))
     };
@@ -300,7 +363,7 @@ fn main() {
 
     // *c = None;
 
-    println!("{:?}", b);
+    println!("{:?}", b);*/
 
 /*    println!("start");
     test_concurrency();
